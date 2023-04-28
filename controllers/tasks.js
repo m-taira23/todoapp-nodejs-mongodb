c53d4a3be3e40cc6bb473094025b1d292d70cdd1
m-taira23 <m.taira@mendoff.jp>
2023-04-28 09:18:51 +0900

    first commit

---
+++ controllers/tasks.js
@@ -0,0 +1,41 @@
+const Task = require("../models/Task");
+
+
+const getAllTasks = async (req, res) => {
+    try {
+        const allTask = await Task.find({});
+        res.status(200).json(allTask);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+};
+
+const createTask = async (req, res) => {
+    try {
+      const createTask = await Task.create(req.body);
+      res.status(200).json(createTask);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+};
+
+const getSingleTask = (req, res) => {
+    res.send("ある特定のタスクを取得しました");
+};
+
+const updateTask = (req, res) => {
+    res.send("ある特定のタスクを更新しました");
+};
+
+const deleteTask = (req, res) => {
+    res.send("ある特定のタスクを削除しました");
+};
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getSingleTask,
+    updateTask,
+    deleteTask,
+};
